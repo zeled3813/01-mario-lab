8754fe733db510ed9035437b04f2193cac0edefe
zeled3813 <zeled3813@hstat.org>
2017-10-08 15:46:49 +0000

    fixed a small error on the height of the pyramid

--- pyramid.rb
+++ pyramid.rb
@@ -1,11 +1,11 @@
 #write your code here
 puts "How tall would you like the pyramid to be? It has to be less than or equal to 23"
 user_input = gets.chomp.to_i
-$level = 1
+$level = 0
 $num = user_input
 $total = $num - $level
 def mario
-    while $level < $num do
+    while $level <= $num do
         puts " "*$total   + "#"*$level + " "*2 + "#"*$level + " "*$total
         $level += 1
         $total = $num - $level
